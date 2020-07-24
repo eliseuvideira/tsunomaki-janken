0db5d589d121de9a1b8c3dd697b1a83d21709604
Eliseu Videira <eliseuvideira1@gmail.com>
2020-07-23 22:02:04 -0300

    Added split script

---
+++ youtube/split.sh
@@ -0,0 +1,3 @@
+ffmpeg -i paper-output.mp4 -ss 06:25.5 -to 06:42.5 -async 1 paper-cut.mp4
+ffmpeg -i scissors-output.mp4 -ss 08:35.8 -to 08:52.8 -async 1 scissors-cut.mp4
+ffmpeg -i rock-output.mp4 -ss 08:33.5 -to 08:50.5 -async 1 rock-cut.mp4
